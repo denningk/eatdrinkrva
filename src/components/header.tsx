b93390e589a2e74d940454c955d5b8dcaa43adf3
Keith Denning <keithdenning1@gmail.com>
2019-06-28 09:48:34 -0400

    Feature/add scss modules (#7)
    * Add SASS to project
    
    * Enabled automatic typing for SCSS modules. Create example moduling for header.
    
    * Add line at end of file

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,11 @@
 import { Link } from "gatsby"
-//import PropTypes from "prop-types"
 import * as React from "react"
+import headerStyles from "./header.module.scss"
 
 const Header = (props: HeaderProps) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <header className={headerStyles.header}>
+    <div className={headerStyles.headerContainer}>
+      <h1>
         <Link
           to="/"
           style={{

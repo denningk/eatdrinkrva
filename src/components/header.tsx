ecae1801a0beb4a88b8c3bfe3b0ffdf296082c09
Keith <keithdenning1@gmail.com>
2019-06-27 22:00:22 -0400

    Enabled automatic typing for SCSS modules. Create example moduling for header.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,11 @@
 import { Link } from "gatsby"
 import * as React from "react"
-import headerStyles from "./header.module.css"
+import headerStyles from "./header.module.scss"
 
 const Header = (props: HeaderProps) => (
   <header className={headerStyles.header}>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+    <div className={headerStyles.headerContainer}>
+      <h1>
         <Link
           to="/"
           style={{
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,7 +9,6 @@ import * as React from "react"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
 
 const Layout = (props: LayoutProps) => (
   <StaticQuery
@@ -25,14 +24,7 @@ const Layout = (props: LayoutProps) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <div>
           <main>{props.children}</main>
           <footer>
             © {new Date().getFullYear()}, Built with

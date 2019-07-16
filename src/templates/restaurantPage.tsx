f83f23bfc716372e6bfb3554e9994b12236df4c4
Keith Denning <denningk18@mail.wlu.edu>
2019-07-16 19:12:35 -0400

    Null check on restaurant description

--- src/templates/restaurantPage.tsx
+++ src/templates/restaurantPage.tsx
@@ -17,7 +17,7 @@ function RestaurantPageTemplate(props: RestaurantPageTemplate) {
       </h3>
       <h1 className={restaurantStyles.restaurant}>{restaurant.name}</h1>
       <h3 className={restaurantStyles.description}>
-        {restaurant.description.internal.content}
+        {restaurant.description && restaurant.description.internal.content}
       </h3>
       <ul>
         {restaurant.happyHours.map((happyHour: any) => (
